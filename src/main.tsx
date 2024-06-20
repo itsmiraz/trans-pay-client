a062eb6def191f346b0f40cceca7b5017bbfaea8
itsmiraz <mirajulislamofficial044@gmail.com>
2024-06-20 10:18:12 +0600

    folder stucture sorted

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store.ts";
 // import { PersistGate } from "redux-persist/integration/react";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./redux/routes/routes.tsx";
+import { router } from "./routes/routes.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
